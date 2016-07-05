44f29deb59a680fb48a3ccdbef52be99b068196c
zhangfeng <227411332@qq.com>
2016-07-05 16:31:40 +0800

    项目  gulp   工具  脚步提交

---
+++ gulpfile.js
@@ -0,0 +1,128 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var bower = require('bower');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var sh = require('shelljs');
+
+var gulify = require('gulp-uglify');
+var paths = {
+  sass: ['./scss/**/*.scss'],
+  customsass:['./customsass/**/*.scss'],
+  appConfig:['./www/js/']
+};
+gulp.task('default', ['sass']);
+gulp.task('sass', function(done) {
+  gulp.src('./scss/ionic.app.scss')
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(gulp.dest('./www/css/'))
+    .pipe(minifyCss({
+      keepSpecialComments: 0
+    }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./www/css/'))
+    .on('end', done);
+});
+
+
+
+
+//编译自定义的样式
+gulp.task('barterstyle',function(){
+    gulp.src('./customsass/main.scss')
+      .pipe(sass())
+      .on('error',sass.logError)
+      .pipe(gulp.dest('./www/css/'))
+      .pipe(minifyCss({
+        keepSpecialComments: 0,
+        advanced: true,
+      }))
+      .pipe(rename({ extname: '.min.css' }))
+      .pipe(gulp.dest('./www/css/'))
+});
+gulp.task('buildStyle',function(){
+  gulp.watch(paths.customsass, ['barterstyle']);
+});
+
+
+//开发环境合并js  代码
+gulp.task('dev_appconfig',function(){
+  gulp.src('./www/js/modular/config/**/*.js')
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('./www/js'))
+});
+
+gulp.task('dev_controllers',function(){
+  gulp.src('./www/js/modular/controller/**/*.js')
+    .pipe(concat('controllers.js'))
+    .pipe(gulp.dest('./www/js'))
+});
+gulp.task('dev_services',function(){
+  gulp.src('./www/js/modular/service/**/*.js')
+    .pipe(concat('services.js'))
+    .pipe(gulp.dest('./www/js'))
+});
+gulp.task('dev_buildJs',function(){
+  gulp.watch('./www/js/modular/config/**/*.js',['dev_appconfig']);
+  gulp.watch('./www/js/modular/controller/**/*.js',['dev_controllers']);
+  gulp.watch('./www/js/modular/service/**/*.js',['dev_services']);
+});
+
+
+
+//生产压缩js代码
+gulp.task('pdc_appconfig',function(){
+  gulp.src('./www/js/modular/config/**/*.js')
+    .pipe(concat('app.js'))
+    .pipe(gulify())
+    .pipe(gulp.dest('./www/js'))
+});
+
+gulp.task('pdc_controllers',function(){
+  gulp.src('./www/js/modular/controller/**/*.js')
+    .pipe(concat('controllers.js'))
+    .pipe(gulify())
+    .pipe(gulp.dest('./www/js'))
+});
+gulp.task('pdc_services',function(){
+  gulp.src('./www/js/modular/service/**/*.js')
+    .pipe(concat('services.js'))
+    .pipe(gulify())
+    .pipe(gulp.dest('./www/js'))
+});
+
+gulp.task('pdc_buildJs',function(){
+  gulp.watch('./www/js/modular/config/**/*.js',['pdc_appconfig']);
+  gulp.watch('./www/js/modular/controller/**/*.js',['pdc_controllers']);
+  gulp.watch('./www/js/modular/service/**/*.js',['pdc_services']);
+});
+
+
+
+
+
+
+gulp.task('watch', function() {
+  gulp.watch(paths.sass, ['sass']);
+});
+gulp.task('install', ['git-check'], function() {
+  return bower.commands.install()
+    .on('log', function(data) {
+      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+    });
+});
+gulp.task('git-check', function(done) {
+  if (!sh.which('git')) {
+    console.log(
+      '  ' + gutil.colors.red('Git is not installed.'),
+      '\n  Git, the version control system, is required to download Ionic.',
+      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
+      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+    );
+    process.exit(1);
+  }
+  done();
+});
