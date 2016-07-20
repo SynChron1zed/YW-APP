2806696590b26f2d1536618afa7f61b0eda98f25
zhangfeng <227411332@qq.com>
2016-07-20 20:13:29 +0800

    test1

--- www/js/modular/config/routeconfig/config.js
+++ www/js/modular/config/routeconfig/config.js
@@ -4,7 +4,7 @@
 App.config(['$stateProvider','$urlRouterProvider','$ionicConfigProvider','$httpProvider','$ionicNativeTransitionsProvider',function($stateProvider,$urlRouterProvider,$ionicConfigProvider,$httpProvider,$ionicNativeTransitionsProvider){
 
   $ionicNativeTransitionsProvider.setDefaultOptions({
-    duration: 400, // in milliseconds (ms), default 400,
+    duration: 401, // in milliseconds (ms), default 400,
     slowdownfactor: 4, // overlap views (higher number is more) or no overlap (1), default 4
     iosdelay: -1, // ms to wait for the iOS webview to update before animation kicks in, default -1
     androiddelay: -1, // same as above but for Android, default -1
