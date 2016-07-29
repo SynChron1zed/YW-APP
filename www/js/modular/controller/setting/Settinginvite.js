b96f7b09288c606bd8f2b7068593aa442fbe5f50
Administrator <admin@admin.com>
2016-07-29 09:13:28 +0800

    测试提交 7.29 好友邀请

--- www/js/modular/controller/setting/Settinginvite.js
+++ www/js/modular/controller/setting/Settinginvite.js
@@ -6,6 +6,31 @@ Ctr.controller('SettinginviteCtr',['$scope','storage','Tools','native','$state',
 
   $scope.Hight =window.innerHeight+"px";
   console.log($scope.Hight)
+  Tools.getData({
+    "interface_number": "050203",
+    "post_content": {
+      "token":"",
+      "token_phone": ""
+
+    }
+
+  },function(r){
+
+
+    if(r.msg== "success"){
+        $scope.one = r.resp_data.one;
+       $scope.count = r.resp_data.total_count;
+       $scope.rebate = r.resp_data.total_rebate
+
+    }else{
+
+      return false
+
+    }
+
+
+  });
+
 
 
 
