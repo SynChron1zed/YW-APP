6efd20aae7d091f2220b495ab44b57a3556e825f
Administrator <admin@admin.com>
2016-08-19 17:27:26 +0800

    测试提交 8.19  诚信企业

--- www/js/modular/controller/login/selectPayduesctr.js
+++ www/js/modular/controller/login/selectPayduesctr.js
@@ -1,56 +1,93 @@
 /**
  * Created by Why on 16/6/8.
  */
-Ctr.controller('selectPayduesctr',['$scope','$state','Tools','$ionicViewSwitcher','$ionicNativeTransitions','$timeout','$ionicHistory','fromStateServ','$stateParams',function($scope,$state,Tools,$ionicViewSwitcher,$ionicNativeTransitions,$timeout,$ionicHistory,fromStateServ,$stateParams){
+Ctr.controller('selectPayduesctr',['$ionicHistory','$scope','$rootScope','$ionicViewSwitcher','$state','$timeout','$ionicNativeTransitions','selectaouthfunl','storage','fromStateServ',function($ionicHistory,$scope,$rootScope,$ionicViewSwitcher,$state,$timeout,$ionicNativeTransitions,selectaouthfunl,storage,fromStateServ){
 
 
 
-   //对安卓返回键的  特殊处理  tabs
- /* $scope.$on('$ionicView.beforeEnter',function(){
-      //注册安卓返回的处理
-      window.androdzerofun  =  function(ba,com){
-          $ionicViewSwitcher.nextDirection('back');
-                $ionicNativeTransitions.stateGo(ba,{},{
-                  "type": "slide",
-                  "direction": "right", // 'left|right|up|down', default 'left' (which is like 'next')
-                  "duration": 400, // in milliseconds (ms), default 400
-                });
-              $timeout(function(){
-                  com();
-                  window.androdzerofun  =   undefined;
-                  window.androdzerofun_parms  =   undefined;
-                  window.androdzerofun_clback  =   undefined;
-              },200)
-      };
-      window.androdzerofun_parms  =    'r.tab.Home';
-      window.androdzerofun_clback  =    function(){
-        $ionicHistory.clearHistory();
-      };
-
-      $scope.GoBackHome =  function(){
-          window.androdzerofun(window.androdzerofun_parms,window.androdzerofun_clback);
-      }
-  })
-*/
+  //对安卓返回键的  特殊处理  tabs
+  $scope.$on('$ionicView.beforeEnter',function(){
 
 
+    if(storage.getObject('UserInfo').need_paid  =='false'){
+      $state.jiaofei  = false;
+    }else{
+      $state.jiaofei  = true;
+    }
 
-  $scope.$on('$ionicView.beforeEnter',function(event, data){
 
-
-    
-    if(fromStateServ.getState('r.selectPaydues')   &&  !$stateParams.inside ){
+    if(fromStateServ.getState('r.selectPaydues')){
       $scope.showtitle  = true;
       $scope.backtoprevView  =   fromStateServ.backView;
       $scope.parenttitle     =   fromStateServ.getState('r.selectPaydues').title;
     }else{
       $scope.showtitle  = false;
     }
-    if(!$scope.parenttitle){
-      $scope.parenttitle  = '返回';
+
+
+
+
+    //注册安卓返回的处理
+    window.androdzerofun  =  function(ba,com){
+
+      if(selectaouthfunl.state){
+        selectaouthfunl.state  = false;
+        $rootScope.$ionicGoBack();
+      }else{
+
+        $ionicViewSwitcher.nextDirection('back');
+        $ionicNativeTransitions.stateGo(ba,{},{
+          "type": "slide",
+          "direction": "right", // 'left|right|up|down', default 'left' (which is like 'next')
+          "duration": 400, // in milliseconds (ms), default 400
+        });
+
+
+        $timeout(function(){
+          com();
+          window.androdzerofun  =   undefined;
+          window.androdzerofun_parms  =   undefined;
+          window.androdzerofun_clback  =   undefined;
+        },200)
+
+      }
+
+    };
+
+
+    window.androdzerofun_parms  =    'r.tab.Home';
+    window.androdzerofun_clback  =    function(){
+      $ionicHistory.clearHistory();
+
+    };
+
+
+
+
+
+    $scope.GoBackHome =  function(){
+      if(selectaouthfunl.state){
+        selectaouthfunl.state  = false;
+        $rootScope.$ionicGoBack();
+      }else{
+        window.androdzerofun(window.androdzerofun_parms,window.androdzerofun_clback);
+      }
+
     }
 
-  });
+
+
+
+
+
+
+
+
+
+  })
+
+
+
 
 
 
